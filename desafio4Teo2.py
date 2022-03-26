9f8481e854cdbcb715a8e070f040e46090e67055
matiasinfo <guitarramamadera@gmail.com>
2022-03-26 10:45:15 -0300

    termino de copletar el desafio 4

--- desafio4Teo2.py
+++ desafio4Teo2.py
@@ -1,16 +1,36 @@
-import string
-
 
 def ingreso_notas():
-""" Esta función retorna un diccionario con los nombres y notas de estudiantes """
-nombre = input("Ingresa un nombre (<FIN> para finalizar)")
-dicci = {}
-while nombre != "FIN":
-    nota = int(input(f"Ingresa la nota de {nombre}"))
-    dicci[nombre] = nota
+    """ Esta función retorna un diccionario con los nombres y notas de estudiantes """
     nombre = input("Ingresa un nombre (<FIN> para finalizar)")
-return dicci
+    dicci = {}
+    while nombre != "FIN":
+        nota = int(input(f"Ingresa la nota de {nombre}: "))
+        dicci[nombre] = nota
+        nombre = input("Ingresa un nombre (<FIN> para finalizar)")
+    return dicci
 
-def 
+
+def calcular_promedio(dicci):
+    """ esta funcion recibe un diccionario y retorna el promedio de las notas"""
+    notas= dicci.values()
+    total = 0
+    for elem in notas:
+        total += elem
+    return total / len(dicci)
+
+
+def reporte(dicci, promedio):
+    """en este metodo se genera una lista de alumnosvde alumnos,
+        que estan por debajo de la nota promedio"""
+    alumnos_complicados = []
+    for elem in dicci:
+        if dicci[elem] < promedio:
+            alumnos_complicados.append(elem)
+    return alumnos_complicados
+
+
 notas_de_estudiantes = ingreso_notas()
-notas_de_estudiantes
+promedio = calcular_promedio(notas_de_estudiantes)
+alumnos = reporte(notas_de_estudiantes, promedio)
+# como tengo que tratar este tipo de lineas que son demaciado largas?
+print(f"el promedio de las notas es: {promedio:5} ,  y los alumnos que tienen notas por debajo del primedio son: {alumnos}")
